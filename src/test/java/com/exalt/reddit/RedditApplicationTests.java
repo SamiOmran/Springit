1efddc8a087f7a7662730041fbb46e9a8a2aeb7f
Sami Imran <samiimran2016@gmail.com>
2021-07-12 15:29:43 +0300

    Initial commit

---
+++ src/test/java/com/exalt/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exalt.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
