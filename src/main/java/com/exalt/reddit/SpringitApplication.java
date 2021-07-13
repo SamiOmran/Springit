b5a2cb57e424efbb675ed8bd4bdae4c5ac8a612e
Sami Imran <samiimran2016@gmail.com>
2021-07-13 15:50:32 +0300

    some changes

--- src/main/java/com/exalt/reddit/SpringitApplication.java
+++ src/main/java/com/exalt/reddit/SpringitApplication.java
@@ -1,14 +1,14 @@
 package com.exalt.reddit;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedditApplication {
+
+public class SpringitApplication {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to this branch");
-        SpringApplication.run(RedditApplication.class, args);
+        System.out.println("Welcome!");
+        SpringApplication.run(SpringitApplication.class, args);
     }
 
 }
